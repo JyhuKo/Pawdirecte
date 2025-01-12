c75c0ab6c5df16d3ae765ca32de7b1d3d8d8d4c1
JyhuKo <github@xbxbot.com>
2025-01-12 23:15:21 +0100

    ig

--- src/models/account-kind.ts
+++ src/models/account-kind.ts
@@ -1,9 +1,20 @@
+// Définition de AccountKind avec exportation constante
 export const AccountKind = {
-  Student: "E"
+  Student: "E",
   // Family1: "1",
-  // Family2: "2"
+  // Family2: "2",
   // Staff: "A",
   // Teacher: "P"
 } as const;
 
+
 export type AccountKind = typeof AccountKind[keyof typeof AccountKind];
+
+
+export type Module = {
+  code: string;
+  enable: boolean;
+  order: number;
+  badge: number;
+  params: Record<string, any>;  
+};
