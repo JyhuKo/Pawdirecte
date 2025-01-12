4c8b64bcf4a4ff342bff496f25125da4df5e4018
JyhuKo <github@xbxbot.com>
2025-01-12 23:36:35 +0100

    Update period.ts

--- src/decoders/period.ts
+++ src/decoders/period.ts
@@ -1,31 +1,35 @@
-export type Period = Readonly<{
-  id: string;
-  name: string;
-  yearly: boolean;
-  isMockExam: boolean;
-  isEnded: boolean;
-  startDate: Date;
-  endDate: Date;
-  councilDate?: Date;
-  councilStartHour?: string;
-  councilEndHour?: string;
-  councilClassroom?: string;
-  ensembleSubjects?: {
-    calculationDate: string;
-    overallAverage?: string;
-    classAverage?: string;
-    minAverage?: string;
-    maxAverage?: string;
-    classRepresentativeName: string;
-    classRepresentativeAppreciation?: string;
-    classSupervisorName: string;
-    classSupervisorAppreciation?: string;
-    supervisorAppreciation?: string;
-    councilDecision: string;
-    rank?: string;
-    classStrength?: string;
-    overallClassAppreciation?: string;
-    subjects: Array<string>;
-    simulatedSubjects: Array<unknown>;
+import type { Period } from "~/models";
+
+export const decodePeriod = (item: any): Period => {
+  return {
+    id: item.idPeriode,
+    name: item.periode,
+    startDate: new Date(item.dateDebut),
+    endDate: new Date(item.dateFin),
+    isEnded: item.cloture,
+    councilDate: new Date(item.dateConseil),
+    councilClassroom: item.salleConseil,
+    councilStartHour: item.heureConseil,
+    councilEndHour: item.heureFinConseil,
+    isMockExam: item.examenBlanc,
+    yearly: item.annuel,
+    ensembleSubjects: {
+      calculationDate: item.ensembleMatieres.dateCalcul,
+      overallAverage: item.ensembleMatieres.moyenneGenerale,
+      classAverage: item.ensembleMatieres.moyenneClasse,
+      minAverage: item.ensembleMatieres.moyenneMin,
+      maxAverage: item.ensembleMatieres.moyenneMax,
+      classRepresentativeName: item.ensembleMatieres.nomPP,
+      classRepresentativeAppreciation: item.ensembleMatieres.appreciationPP,
+      classSupervisorName: item.ensembleMatieres.nomCE,
+      classSupervisorAppreciation: item.ensembleMatieres.appreciationCE,
+      supervisorAppreciation: item.ensembleMatieres.appreciationVS,
+      councilDecision: item.ensembleMatieres.decisionDuConseil,
+      rank: item.ensembleMatieres.rang,
+      classStrength: item.ensembleMatieres.effectif,
+      overallClassAppreciation: item.ensembleMatieres.appreciationGeneraleClasse,
+      subjects: item.ensembleMatieres.disciplines,
+      simulatedSubjects: item.ensembleMatieres.disciplinesSimulation
+    }
   };
-}>;
+};
