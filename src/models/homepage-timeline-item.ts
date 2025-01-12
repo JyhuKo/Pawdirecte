0014efd57f915b10bd68b06ce3e139f68b483d78
JyhuKo <github@xbxbot.com>
2025-01-12 22:51:26 +0100

    Update homepage-timeline-item.ts

--- src/models/homepage-timeline-item.ts
+++ src/models/homepage-timeline-item.ts
@@ -2,8 +2,13 @@ export type HomepageTimelineItem = Readonly<{
   id: string;
   content: string;
   authorName: string;
+  authorId: string; 
+  authorFirstName: string;  
+  authorTitle: string; 
   creationDate: Date;
   startDate: Date;
   endDate: Date;
+  target: any[];  
+  targetSchools: any[];  
   colorName: string;
 }>;
